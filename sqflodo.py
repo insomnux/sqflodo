8e405ce4b563844558beb657c26ae87c33a71fde
insomnux <insomnux@insomnus.info>
2018-11-15 21:04:54 +0200

    removed 'new' route

--- sqflodo.py
+++ sqflodo.py
@@ -1,24 +1,24 @@
 import os.path, datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 tasklist = []
 
 # Views
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
-
-@app.route("/new", methods=["POST"])
-def new():
     if request.method == "POST":      
         newtask = request.form.get("task[newtask]")
         duedate = request.form.get("task[duedate]")
         tasklist.append([newtask, duedate])
         taskdict = dict(tasklist)
+    else:
+        tasklist.clear()
+        taskdict = {}
 
     return render_template("index.html", taskdict = taskdict.items())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
