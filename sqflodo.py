e9089835521f2cad4339cd95c4d71c6ab1124115
insomnux <insomnux@insomnus.info>
2018-11-15 20:13:52 +0200

    added basic task input and displaying without database

--- sqflodo.py
+++ sqflodo.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+import os.path, datetime
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Views
+tasklist = []
 
+# Views
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/new", methods=["POST"])
+def new():
+    if request.method == "POST":      
+        newtask = request.form.get("task[newtask]")
+        duedate = request.form.get("task[duedate]")
+        tasklist.append([newtask, duedate])
+        taskdict = dict(tasklist)
+
+    return render_template("index.html", taskdict = taskdict.items())
 
 if __name__ == "__main__":
     app.run(debug=True)
